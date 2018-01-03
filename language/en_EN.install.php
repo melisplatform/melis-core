78947530319fcc7548526f61e1dc92ff14d006b1
jerremeirago <jrago@melistechnology.com>
2018-01-03 15:37:38 +0800

    added tooltip on every input of the form

--- language/en_EN.install.php
+++ language/en_EN.install.php
@@ -7,5 +7,13 @@ return array(
     'tr_Melis_installer_new_user_confirm_password' => 'Confirm password',
     'tr_melis_installer_new_user_first_name' => 'First name',
     'tr_melis_installer_new_user_last_name' => 'Last name',
+
+    //toolTip form
+    'tr_melis_installer_new_user_login_info' => 'Username',
+    'tr_melis_installer_new_user_email_info' => 'Email',
+    'tr_melis_installer_new_user_password_info' => 'Password',
+    'tr_Melis_installer_new_user_confirm_password_info' => 'Confirm password',
+    'tr_melis_installer_new_user_first_name_info' => 'Firstname',
+    'tr_melis_installer_new_user_last_name_info' => 'Lastname'
 );
 ?>
