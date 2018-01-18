0d1ec1f73fcb95c71a31248b2ce9bee99e815f3e
Ian Borla <benborla@icloud.com>
2018-01-18 15:22:30 +0800

    modified setup controller

--- src/Controller/MelisSetupController.php
+++ src/Controller/MelisSetupController.php
@@ -55,24 +55,30 @@ class MelisSetupController extends AbstractActionController
             $userFirstname  = $form->get('firstname');
             $userLastname   = $form->get('lastname');
 
-            //Save userData
-            $tableUser->save(array(
-                'usr_status'        => 1,
-                'usr_login'         => $userLogin,
-                'usr_email'         => $userEmail,
-                'usr_password'      => $password,
-                'usr_firstname'     => $userFirstname,
-                'usr_lastname'      => $userLastname,
-                'usr_lang_id'       => 1,
-                'usr_admin'         => 0,
-                'usr_role_id'       => 1,
-                'usr_rights'        => '<?xml version="1.0" encoding="UTF-8"?><document type="MelisUserRights" author="MelisTechnology" version="2.0"><meliscms_pages> <id>-1</id></meliscms_pages><meliscore_interface></meliscore_interface><meliscore_tools> <id>meliscore_tools_root</id></meliscore_tools></document>',
-                'usr_creation_date' => date('Y-m-d H  '),
-            ));
 
-            $success = 1;
-            $message = 'tr_install_setup_message_ok';
 
+            $container = new \Zend\Session\Container('melismodules');
+            $installerModuleConfigurationSuccess = isset($container['module_configuration']['success']) ?
+                (bool) $container['module_configuration']['success'] : false;
+
+            if($installerModuleConfigurationSuccess) {
+                $tableUser->save(array(
+                    'usr_status'        => 1,
+                    'usr_login'         => $userLogin,
+                    'usr_email'         => $userEmail,
+                    'usr_password'      => $password,
+                    'usr_firstname'     => $userFirstname,
+                    'usr_lastname'      => $userLastname,
+                    'usr_lang_id'       => 1,
+                    'usr_admin'         => 0,
+                    'usr_role_id'       => 1,
+                    'usr_rights'        => '<?xml version="1.0" encoding="UTF-8"?><document type="MelisUserRights" author="MelisTechnology" version="2.0"><meliscms_pages> <id>-1</id></meliscms_pages><meliscore_interface></meliscore_interface><meliscore_tools> <id>meliscore_tools_root</id></meliscore_tools></document>',
+                    'usr_creation_date' => date('Y-m-d H  '),
+                ));
+
+                $success = 1;
+                $message = 'tr_install_setup_message_ok';
+            }
         }
         else {
             $errors = $this->formatErrorMessage($form->getMessages());
