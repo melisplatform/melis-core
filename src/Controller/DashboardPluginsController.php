1aefbd9da5a018bc74df4a16975e762bf1ba5f52
Bryan Brioso <rbbrioso28@gmail.com>
2018-03-26 18:44:11 +0800

    Partial dashboard ajax request after drag to dragdropzone

--- src/Controller/DashboardPluginsController.php
+++ src/Controller/DashboardPluginsController.php
@@ -44,6 +44,52 @@ class DashboardPluginsController extends AbstractActionController
     public function getPluginAction()
     {
         // return plugin view
+        $request = $this->getRequest();
+        $postVals = $request->getPost();
+        
+        $pluginManager = $this->getServiceLocator()->get('ControllerPluginManager');
+        $viewRender = $this->getServiceLocator()->get('ViewRenderer');
+        
+        $module = 'meliscore';
+        $pluginName = 'MelisCoreDashboardRecentUserActivityPlugin';
+        $dashboardid = 'id_meliscore_center_dashboard';
+        $pluginid = 'RecentUserActivity_2';
+        
+        $plugin = $pluginManager->get($pluginName);
+        $pluginModel = $plugin->render(
+            array(
+                'dashboard_id' => $dashboardid,
+                'plugin_id' => $pluginid
+            )
+        );
+        
+        $html = $viewRender->render($pluginModel);
+        
+        $jsCallBacks = array();
+        $datasCallback = array();
+        
+        $config = $this->getServiceLocator()->get('config');
+        
+        $pluginConfig = $config['plugins'][$module]['dashboard_plugins'][$pluginName];
+        
+        if (!empty($pluginConfig['interface']) && is_array($pluginConfig['interface']))
+        {
+            $melisAppConfig = $this->getServiceLocator()->get('MelisCoreConfig');
+            list($jsCallBacks, $datasCallback) = $melisAppConfig->getJsCallbacksDatas($pluginConfig);
+        }
+        
+        if (!empty($pluginConfig['jscallback']))
+        {
+            array_push($jsCallBacks, $pluginConfig['jscallback']);
+        }
+        
+        $data = array(
+            'html' => $html,
+            'jsCallbacks' => $jsCallBacks,
+            'jsDatas' => $datasCallback
+        );
+        
+        return new JsonModel($data);
     }
     
     public function savePluginAction()
