76dfeeaa899380d1239b8142e1c6227991acac92
Bryan Brioso <rbbrioso28@gmail.com>
2018-03-28 17:21:19 +0800

    Dashboard plugin menu listener avoided ajax request

--- src/Listener/MelisCoreDashboardMenuListener.php
+++ src/Listener/MelisCoreDashboardMenuListener.php
@@ -24,53 +24,56 @@ class MelisCoreDashboardMenuListener extends MelisCoreGeneralListener implements
         $callBackHandler = $events->attach(
             MvcEvent::EVENT_DISPATCH,
             function(MvcEvent $e){
-                
-                // Get route match to know if we are displaying in back or front
-                $routeMatch = $e->getRouteMatch();
-                
-                $sm = $e->getApplication()->getServiceManager();
-                
-                $view = $e->getApplication()->getMvcEvent()->getViewModel();
-                
-                $plugins = array();
-                
-                $config = $sm->get('config');
-                
-                foreach ($config['plugins'] As $key => $val)
+                // Avoid ajax request
+                if (!$e->getApplication()->getMvcEvent()->getRequest()->isXmlHttpRequest())
                 {
-                    if (!empty($val['dashboard_plugins']))
+                    // Get route match to know if we are displaying in back or front
+                    $routeMatch = $e->getRouteMatch();
+                    
+                    $sm = $e->getApplication()->getServiceManager();
+                    
+                    $view = $e->getApplication()->getMvcEvent()->getViewModel();
+                    
+                    $plugins = array();
+                    
+                    $config = $sm->get('config');
+                    
+                    foreach ($config['plugins'] As $key => $val)
                     {
-                        $plugins[$key] = array();
-                        
-                        $modulePlugins = $val['dashboard_plugins'];
-                        
-                        foreach ($modulePlugins As $keyPlugin => $plugin)
+                        if (!empty($val['dashboard_plugins']))
                         {
-                            if ($keyPlugin != 'MelisCoreDashboardDragDropZonePlugin')
+                            $plugins[$key] = array();
+                            
+                            $modulePlugins = $val['dashboard_plugins'];
+                            
+                            foreach ($modulePlugins As $keyPlugin => $plugin)
                             {
-                                $plugins[$key][$keyPlugin] = array(
-                                    'plugin-module'         => $keyPlugin,
-                                    'plugin-name'           => !empty($plugin['name'])          ? $plugin['name'] : $keyPlugin,
-                                    'plugin-id'             => !empty($plugin['plugin_id'])     ? $plugin['plugin_id']  : '',
-                                    'plugin-width'          => !empty($plugin['width'])         ? $plugin['width']  : '',
-                                    'plugin-height'         => !empty($plugin['height'])        ? $plugin['height']  : '',
-                                    'plugin-description'    => !empty($plugin['description'])   ? $plugin['description']  : '',
-                                    'plugin-icon'           => !empty($plugin['icon'])          ? $plugin['icon'] : '',
-                                    'plugin-thumbnail'      => !empty($plugin['thumbnail'])     ? $plugin['thumbnail'] : '/MelisCore/plugins/images/default.jpg',
-                                    'plugin-is-drag'        => true,
-                                );
+                                if ($keyPlugin != 'MelisCoreDashboardDragDropZonePlugin')
+                                {
+                                    $plugins[$key][$keyPlugin] = array(
+                                        'plugin-module'         => $keyPlugin,
+                                        'plugin-name'           => !empty($plugin['name'])          ? $plugin['name'] : $keyPlugin,
+                                        'plugin-id'             => !empty($plugin['plugin_id'])     ? $plugin['plugin_id']  : '',
+                                        'plugin-width'          => !empty($plugin['width'])         ? $plugin['width']  : '',
+                                        'plugin-height'         => !empty($plugin['height'])        ? $plugin['height']  : '',
+                                        'plugin-description'    => !empty($plugin['description'])   ? $plugin['description']  : '',
+                                        'plugin-icon'           => !empty($plugin['icon'])          ? $plugin['icon'] : '',
+                                        'plugin-thumbnail'      => !empty($plugin['thumbnail'])     ? $plugin['thumbnail'] : '/MelisCore/plugins/images/default.jpg',
+                                        'plugin-is-drag'        => true,
+                                    );
+                                }
                             }
                         }
                     }
+                    
+                    $menu = new ViewModel();
+                    $menu->setTemplate('melis-core/dashboard-plugin/dashboard-menu');
+                    $menu->setVariable('plugins', $plugins);
+                    
+                    $view->addChild($menu, 'dashboardMenu');
+                    
+                    $e->getApplication()->getMvcEvent()->setViewModel($view);
                 }
-                
-                $menu = new ViewModel();
-                $menu->setTemplate('melis-core/dashboard-plugin/dashboard-menu');
-                $menu->setVariable('plugins', $plugins);
-                
-                $view->addChild($menu, 'dashboardMenu');
-                
-                $e->getApplication()->getMvcEvent()->setViewModel($view);
             });
         
         $this->listeners[] = $callBackHandler;
