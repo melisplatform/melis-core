1f67748e301e214132409fa669aaa76ffd3c15c1
James Quibido <ninojamesquibido@gmail.com>
2018-04-04 13:17:34 +0800

    save drop plugin

--- public/js/core/gridstack.init.js
+++ public/js/core/gridstack.init.js
@@ -17,7 +17,7 @@
 
             $('.grid-stack').gridstack(options);
             this.widgetDrag(".melis-core-dashboard-plugin-snippets");
-            this.dropWidget();
+            this.dropWidget(target);
             this.changeWidget();
         },
 
@@ -29,56 +29,101 @@
             });
         },
 
-        dropWidget: function() {
-            var active_grid = $('.tab-pane .grid-stack').data('gridstack');
+        dropWidget: function(res) {
+
+            var active_grid = $('#'+activeTabId+' .tab-pane .grid-stack').data('gridstack');
             var gridDrop = active_grid.container.droppable({
                 // accept: ".melis-core-dashboard-plugin-snippets",
                 tolerance: 'pointer',
                 drop: function( event, ui ) {
-                    var request = $.get( "/melis/MelisCore/DashboardPlugins/getPlugin");
+                    var dataString = new Array;
+                    // create dashboard array
+                    dataString.push({
+                        name: 'dashboard_id',
+                        value: activeTabId
+                    });
+
+                    // get plugin menu data
+                    var pluginMenu = $(ui.helper[0]).find(".plugin-json-config").text();
+
+                    // check plugin menu
+                    if(pluginMenu) {
+                        // parse to JSON
+                        var pluginConfig = JSON.parse(pluginMenu);
+                        $.each(pluginConfig, function(index, value){
+                            // check and modify w h value
+                            if(index == "width" && value == "") { value = 6 };
+                            if(index == "height" && value == "") { value = 6 };
+                            // push to dashboard array
+                            dataString.push({
+                                name: index,
+                                value: value
+                            });
+                        });
+                    }
+
+
+                    var request = $.post( "/melis/MelisCore/DashboardPlugins/getPlugin", dataString);
                     request.done(function(data){
-                        // adding Widget
-                        var grid = $('.grid-stack').data('gridstack');
+                        // get dashboard gridstack data
+                        var grid = $('#'+activeTabId+' .grid-stack').data('gridstack');
+                        // get placeholder data
                         var gridData = grid.placeholder.data();
-                        console.log('data ', $(data.html));
                         var html = $(data.html);
+                        // add widget to dashboard
                         var widget = grid.addWidget(html, gridData.gsX, gridData.gsY, 6, 6);
 
                         // remove clone widgets
-                        grid.removeWidget($(widget).prev());
-
-
+                         grid.removeWidget($(widget).prev());
                     });
-
                 }
             });
         },
 
         serializeWidgetMap: function(items) {
+
+            var dataString = new Array;
+            // create dashboard array
+            dataString.push({
+                name: 'dashboard_id',
+                value: activeTabId
+            });
+
             $.each(items, function(key, value) {
-                var dataString = $(items[key].el).find('.hidden').text();
-                if(dataString) {
-                    // send dashboard lists
-                    console.log('items ', JSON.parse(dataString) );
+                var dataTxt = $(items[key].el).find('.dashboard-plugin-json-config').text();
+                // check dashboard data
+                if(dataTxt) {
+                    // get dynamic dashboard value
+                    var dashboardX = value.x;
+                    var dashboardY = value.y;
+                    var dashboardWidth = value.width;
+                    var dashboardHeight = value.height;
+                    // JSON parse dashboard txt
+                    var pluginConfig = JSON.parse(dataTxt);
+                    $.each(pluginConfig, function(index, value){
+                        // here modify x y w h of the plugin
+                        if(index == "x-axis") { value = dashboardX; }
+                        if(index == "y-axis") { value = dashboardY; }
+                        if(index == "width") { value = dashboardWidth; }
+                        if(index == "height") { value = dashboardHeight; }
+                        // push to dashboard array
+                        dataString.push({
+                            name: 'plugins['+pluginConfig["plugin"]+']['+pluginConfig["plugin_id"]+']['+index+']',
+                            value: value
+                        });
+                    });
                 }
 
             });
 
-            /*postDashboardNew.done(function(data) {
-             console.log('Success ', data);
-             });
-
-             postDashboardNew.fail(function(error) {
-             console.log('Something went wrong ', error);
-             });*/
+            // save dashboard lists
+            var saveDashboardLists = $.post("/melis/MelisCore/DashboardPlugins/saveDashboardPlugins", dataString);
         },
 
         changeWidget: function() {
             var self = this;
             // gridstack change
             $('.grid-stack').on('change', function(event, items) {
-                console.log('event change ', event);
-                console.log('items change ', items);
                 // hide plugin menu
                 $(".melis-core-dashboard-dnd-box").removeClass("shown");
                 self.serializeWidgetMap(items);
@@ -86,6 +131,6 @@
         },
     };
 
-
+// init
 melisDashBoardDragnDrop.init();
 
