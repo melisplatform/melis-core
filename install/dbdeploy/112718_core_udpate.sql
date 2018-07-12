9d944382a443d820d49a84c6a0190925d55fde16
jerremeirago <jrago@melistechnology.com>
2018-07-12 20:03:41 +0800

    updated default scheme colors of melisplatform

---
+++ install/dbdeploy/112718_core_udpate.sql
@@ -0,0 +1,6 @@
+-- -----------------------------------------------------
+-- Update default MelisPlatform colors `melis_core_platform_scheme`
+-- -----------------------------------------------------
+START TRANSACTION;
+UPDATE `melis_core_platform_scheme` SET `pscheme_colors`= '{\"melis_core_platform_color_primary_color\":\"#e61c23\",\"melis_core_platform_color_secondary_color\":\"#c52127\"}' where pscheme_id = 1;
+COMMIT;
