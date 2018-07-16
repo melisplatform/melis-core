1830cfe5ede122e45a263dce9ad93122ad94368e
jerremeirago <jrago@melistechnology.com>
2018-07-16 17:03:42 +0800

    modify sql file

--- install/dbdeploy/112718_core_udpate.sql
+++ install/dbdeploy/112718_core_udpate.sql
@@ -3,4 +3,6 @@
 -- -----------------------------------------------------
 START TRANSACTION;
 UPDATE `melis_core_platform_scheme` SET `pscheme_colors`= '{\"melis_core_platform_color_primary_color\":\"#e61c23\",\"melis_core_platform_color_secondary_color\":\"#c52127\"}' where pscheme_id = 1;
+UPDATE `melis_core_platform_scheme` SET `pscheme_colors`= '{\"melis_core_platform_color_primary_color\":\"#e61c23\",\"melis_core_platform_color_secondary_color\":\"#c52127\"}' where pscheme_id = 2;
+
 COMMIT;
