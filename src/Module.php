1be0397ad724e92e029fbed6931431b7ad76993f
bryan <rbbrioso28@gmail.com>
2018-08-13 18:58:11 +0800

    New lable on dashboard menu

--- src/Module.php
+++ src/Module.php
@@ -263,6 +263,7 @@ class Module
 			include __DIR__ . '/../config/module.config.php',
 			include __DIR__ . '/../config/app.interface.php',
 			include __DIR__ . '/../config/app.tools.php',
+			include __DIR__ . '/../config/app.toolstree.php',
             include __DIR__ . '/../config/app.interface.general.php',
 			include __DIR__ . '/../config/app.forms.php',
 	        include __DIR__ . '/../config/app.tools.php',
