e2e037f54accf26376389609dca221739eb4be66
Cris <crismakabenta@gmail.com>
2018-12-13 15:25:15 +0800

    refactored plugin config

--- config/dashboard-plugins/MelisCoreDashboardRecentUserActivityPlugin.config.php
+++ config/dashboard-plugins/MelisCoreDashboardRecentUserActivityPlugin.config.php
@@ -8,30 +8,40 @@
                     'js' => array(
                     )
                 ),
-                'dashboard_plugins' => array(
-                    'MelisCoreDashboardRecentUserActivityPlugin' => array(
-                        'plugin_id' => 'RecentUserActivity',
-                        'name' => 'tr_meliscore_dashboard_Recent Activity',
-                        'description' => 'tr_meliscore_dashboard_Recent Activity description',
-                        'icon' => 'fa fa-users',
-                        'thumbnail' => '/MelisCore/plugins/images/MelisCoreDashboardRecentUserActivityPlugin.jpg',
-                        'jscallback' => '',
-                        'max_lines' => 8,
-                        'height' => 4,
-                        
-                        'interface' => array(
-                            'meliscore_dashboard_recent_activity_users' => array(
-                                'forward' => array(
-                                    'module' => 'MelisCore',
-                                    'plugin' => 'MelisCoreDashboardRecentUserActivityPlugin',
-                                    'function' => 'recentActivityUsers',
-                                    'jscallback' => '',
-                                    'jsdatas' => array()
-                                ),
-                            ),
-                        )
-                    )
-                ),
+                'interface' => [
+                    'meliscore_leftmenu' => [
+                        'interface' => [
+                            'melisdashboardplugin_section' => [
+                                'interface' => [
+
+                                    'MelisCoreDashboardRecentUserActivityPlugin' => array(
+                                        'plugin_id' => 'RecentUserActivity',
+                                        'name' => 'tr_meliscore_dashboard_Recent Activity',
+                                        'description' => 'tr_meliscore_dashboard_Recent Activity description',
+                                        'icon' => 'fa fa-users',
+                                        'thumbnail' => '/MelisCore/plugins/images/MelisCoreDashboardRecentUserActivityPlugin.jpg',
+                                        'jscallback' => '',
+                                        'max_lines' => 8,
+                                        'height' => 4,
+
+                                        'interface' => array(
+                                            'meliscore_dashboard_recent_activity_users' => array(
+                                                'forward' => array(
+                                                    'module' => 'MelisCore',
+                                                    'plugin' => 'MelisCoreDashboardRecentUserActivityPlugin',
+                                                    'function' => 'recentActivityUsers',
+                                                    'jscallback' => '',
+                                                    'jsdatas' => array()
+                                                ),
+                                            ),
+                                        )
+                                    )
+
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
             )
         ),
     );
