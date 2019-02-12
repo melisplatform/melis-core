493794b2a2408b3ea02d9dff9bd56df928a14188
Junry Zabate <jzabajte@melistechnology.com>
2019-02-12 13:28:28 +0800

    updated tinymce file source to v. 5

--- public/js/tinyMCE/tool.php
+++ public/js/tinyMCE/tool.php
@@ -12,9 +12,10 @@
 	    'setup' => 'melisTinyMCE.tinyMceActionEvent',
 	    'paste_auto_cleanup_on_paste' => true,
 		'plugins' => array(
+			//[contextmenu, textcolor, colorpicker] this plugin is already built in the core editor as of TinyMCE v. 5
 		    'advlist autolink lists link image charmap print preview anchor',
 		    'searchreplace visualblocks code fullscreen',
-		    'insertdatetime media table contextmenu paste'
+		    'insertdatetime media table paste'
 	    ),
 	    'toolbar' => 'undo redo | styleselect | bold italic | link image |  alignleft aligncenter alignright alignjustify',
 	    'init_instance_callback' => 'tinyMceCleaner',
