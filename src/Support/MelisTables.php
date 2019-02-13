720fcec74b5fe46f504ea5ae206ace6cc1532246
benborla <benborla@icloud.com>
2019-02-13 12:45:00 +0800

    updated constants

--- src/Support/MelisTables.php
+++ src/Support/MelisTables.php
@@ -10,10 +10,38 @@ namespace MelisCore\Support;
  */
 class MelisTables
 {
+    /**
+     * @const RELATIONAL_DATA holds the string value of 'relational_data"
+     */
     const RELATIONAL_DATA = 'relational_data';
+
+    /**
+     * @const RELATION holds the string value of "relation"
+     */
     const RELATION = 'relation';
+
+    /**
+     * @const QUERY holds the string value of "query"
+     */
     const QUERY = 'query';
+
+    /**
+     * @const SQL holds the string value of "sql"
+     */
     const SQL = 'sql';
+
+    /**
+     * @const FOREIGN_KEY holds the string value of "%foreign_key%" which will be used to swap to a real foreign key value
+     */
     const FOREIGN_KEY = '%foreign_key%';
+
+    /**
+     * @const FOREIGN_KEY holds the string value of "%root_foreign_key%" which will be used to swap to the data map root parent foreign key
+     */
     const ROOT_FOREIGN_KEY = '%root_foreign_key%';
+
+    /**
+     * @const AUTO_INCREMENT holds the string value of "%auto_increment%" which will be used to swap to a real value
+     */
+    const AUTO_INCREMENT = '%auto_increment';
 }
