bb5b3ddd3cdb48761eb7f96fdf3ec73d1dcea037
benborla <benborla@icloud.com>
2019-02-19 19:18:30 +0800

    updated configuration

--- config/setup/update.config.php
+++ config/setup/update.config.php
@@ -2,13 +2,16 @@
 
 namespace MelisCore;
 
+use MelisMarketPlace\Support\MelisMarketPlace as MarketPlace;
+use MelisMarketPlace\Support\MelisMarketPlaceCmsTables as Melis;
+use MelisMarketPlace\Support\MelisMarketPlaceSiteInstall as Site;
+
 return [
     'plugins' => [
         __NAMESPACE__ => [
-            'setup' => [
-               
+            MarketPlace::UPDATE => [
+
             ]
         ]
     ],
-
 ];
