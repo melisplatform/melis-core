92fef505c8fda97a88fd28c034fd32c584449fe8
Junry Zabate <jzabajte@melistechnology.com>
2019-04-23 20:16:11 +0800

    code update on tinymce

--- public/build/js/bundle.js
+++ public/build/js/bundle.js
@@ -25033,41 +25033,14 @@ var melisTinyMCE = (function(){
         editor.on("init",function() {
             // adding of add tree view button from dialog initialization
             tinyMceDialogInitAddTreeViewBtn(editor);
-
-            // tinymce outside iframe container
-            //tinyMceOnEditorInitToolbarBtn();
         });
-    }
-
-    // adding of specific class on tinymce outside iframe
-    function tinyMceOnEditorInitToolbarBtn() {
-        var $body           = window.parent.$("body"),
-            $btiny          = $body.find(".tox-tinymce"),
-            $baux           = $body.find(".tox-tinymce-aux"),
-            $btoxToolGroup  = $btiny.find(".tox-toolbar .tox-toolbar__group");
-
-        var transTitle      = translations.tr_meliscore_tinymce_insert_edit_link_dialog_title;
-
-            $btoxToolGroup.each(function(i) {
-                var $bBtn = $(this).find("button");
-                    $bBtn.each(function(i) {
-                        var $attrTitle  = $(this)[i].attributes[1].value;
-                            if ( transTitle !== "" && $attrTitle === transTitle ) {
-                                $(this).addClass("insert-edit-link");
-
-                                $(this).on("click", function() {
-                                    tinyMceAddTreeViewBtn();
-                                });
-                            }
-                            return false;
-                    });
-            });
     }
 
     // adding of add tree view button from dialog initialization
     function tinyMceDialogInitAddTreeViewBtn(editor) {
         var $body       = $("body"),
-            transTitle  = translations.tr_meliscore_tinymce_insert_edit_link_dialog_title;
+            transTitle  = translations.tr_meliscore_tinymce_insert_edit_link_dialog_title,
+            $dialog     = $body.find(".tox-dialog");
 
             editor.windowManager.oldOpen = editor.windowManager.open;  // save for later
             editor.windowManager.open = function (t, r) {    // replace with our own function
@@ -25083,31 +25056,25 @@ var melisTinyMCE = (function(){
                         });
                     }
 
+                var $dialog = $(".tox-dialog__header").closest(".tox-dialog");
+
+                    if ( $dialog.length ) {
+
+                        // modal pop up
+                        // window.parent.melisCms.modalPopUp(); // in melisCms.js but not used
+
+                        modalPopUp();
+                    }
+
+
                     return modal; // Template plugin is dependent on this return value
             };
     }
 
-    // adding of tree view button from outside iframe
-    function tinyMceAddTreeViewBtn() {
-        setTimeout(function() {
-            var $body           = window.parent.$("body"),
-                $baux           = $body.find(".tox-tinymce-aux"),
-                $bdialog        = $baux.find(".tox-dialog"),
-                $bconHStacks    = $bdialog.find(".tox-form__controls-h-stack"),
-                $burlBtn        = $bconHStacks.find("#mce-link-tree");
-
-            var melisTreeBtn    = '<button title="Site tree view" id="mce-link-tree" class="mce-btn mce-open" style="width: 34px; height: 34px;"><i class="icon icon-sitemap fa fa-sitemap" style="font-family: FontAwesome; position: relative; font-size: 16px; display: block; text-align: center;"></i></button>';
-                
-                if ( $baux.length ) {
-                    if ( $burlBtn.length === 0 && $bconHStacks.length > 0 ) {
-                        $bconHStacks.append( melisTreeBtn );    
-                    }
-                }
-        }, 50);
-    }
+    
     
     // Stating zone to loading
-    function loadingZone(targetElem){
+    function loadingZone(targetElem) {
         if(targetElem.length){
             var tempLoader = '<div id="loadingZone" class="overlay-loader"><img class="loader-icon spinning-cog" src="/MelisCore/assets/images/cog12.svg" data-cog="cog12"></div>';
             targetElem.attr("style", "position: relative");
@@ -25116,57 +25083,44 @@ var melisTinyMCE = (function(){
     }
     
     // Removing loading state on zone
-    function removeLoadingZone(targetElem){
+    function removeLoadingZone(targetElem) {
         if(targetElem.length){
             targetElem.find("#loadingZone").remove();
         }
     }
 
+    // modal pop up tinymce melis-core
     function modalPopUp() {
-        setTimeout(function() {
-            var $body   = window.parent.$("body"),
-                $btn    = $body.find(".tox-tbtn");
-                // OPENING THE POPUP
-                /*$body.on("click", ".mce-btn", function(){
-                    var mcePopUp = $("#mce-modal-block").length;
-                    if(mcePopUp){
+        // OPENING THE POPUP
+        var $body           = $("body"),
+            mcePopUp        = $body.find(".tox-tinymce-aux").length, // #mce-modal-block [.tox-tinymce-aux]
+            $dialog         = $body.find(".tox-dialog"),
+            $iframe         = window.parent.$(".melis-iframe"),
+            $iframeOffset   = $iframe.position().top;
 
-                        // iframe height
-                        var iframeHeight = $(window).height();
-                            
-                        // dialog box height
-                        var dialogHeight = $(".mce-window").outerHeight();
-                        
-                        parent.scrollToViewTinyMCE(dialogHeight, iframeHeight);
-                        
-                        // CLOSING THE POPUP
-                        var timeOut = setInterval(function(){ 
-                        
-                            // console.log( "return =" + parent.scrollOffsetTinyMCE() );
-                            if( !$(".mce-window").is(":visible") ){
-                            // window.parent.scrollTo( 0,parent.scrollOffsetTinyMCE())
-                                window.parent.$("body").animate({scrollTop: parent.scrollOffsetTinyMCE() }, 200);
-                                
-                                clearTimeout(timeOut);
-                            }
-                        }, 300);
-                        
+            /*console.log("$iframe: ", $iframe);
+            console.log("$iframeOffset: ", $iframeOffset);*/
+
+            if ( mcePopUp ) {
+                // iframe height
+                var iframeHeight = $(window).height(),
+                    // dialog box height .mce-window [.dialog]
+                    dialogHeight = $dialog.outerHeight() - ( $iframeOffset * 10 );
+
+                    /*console.log("dialogHeight: ", dialogHeight);
+                    console.log("$dialog outerHeight(): ", $dialog.outerHeight());
+                    console.log("$iframeOffset: ", $iframeOffset * 10);*/
+
+                    parent.scrollToViewTinyMCE(dialogHeight, iframeHeight);
+                
+                // CLOSING THE POPUP
+                var timeOut = setInterval(function() { 
+                    if( ! $dialog.is(":visible") ) {
+                        window.parent.$("body").animate({scrollTop: parent.scrollOffsetTinyMCE() }, 200);
+                        clearTimeout(timeOut);
                     }
-                    else {
-                        console.log("no popup");
-                    }
-                });*/
-
-                console.log("$btn length: ", $btn.length);
-
-                /*
-                 * [.mce-btn] @ .tox-tbtn
-                 * [#mce-modal-block] @ .tox-tinymce-aux
-                 */
-                $body.on("click", ".tox-tbtn", function() {
-                    console.log(".tox-tbtn");
-                });
-        }, 500);
+                }, 300);
+            }
     }
 
     function addMelisCss() {
@@ -25192,7 +25146,7 @@ var melisTinyMCE = (function(){
     // adding Melis TinyMCE CSS
     melisTinyMCE.addMelisCss();
     // custom modal TinyMCE
-    melisTinyMCE.modalPopUp();
+    //melisTinyMCE.modalPopUp();
 })();
 
 function tinyMceCleaner(editor) {
