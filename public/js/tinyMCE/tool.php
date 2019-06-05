b0351beba7b5972f3818cc71fdace5738bf36fa0
Junry Zabate <jzabajte@melistechnology.com>
2019-06-05 18:37:31 +0800

    added button to show responsive main tabs on scroll stopped and scrollTop equals zero and checks jquery.tinymce.min.js theme is silver also fixes bullets and numbering on tinymce that uses tool.php config not fixed on melis-cms tinymce configs

--- public/js/tinyMCE/tool.php
+++ public/js/tinyMCE/tool.php
@@ -17,7 +17,7 @@
 	    'paste_auto_cleanup_on_paste' => true,
 		'plugins' => array(
             //[contextmenu, textcolor, colorpicker] this plugin is already built in the core editor as of TinyMCE v. 5
-           'advlist autolink lists link paste image charmap preview anchor emoticons help hr nonbreaking',
+           'lists advlist autolink link paste image charmap preview anchor emoticons help hr nonbreaking',
            'searchreplace visualblocks code fullscreen',
            'insertdatetime media table template'
         ),
