c9df3d66483073f95936dfdf58c0ed8912543d7c
bryan <rbbrioso28@gmail.com>
2019-10-24 17:28:34 +0800

    Email mngt form error msgs translation

--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -460,7 +460,8 @@ return [
     'tr_emails_management_unable_to_update' => 'Impossible de mettre à jour l\'email',
     'tr_emails_management_unable_to_add' => 'Impossible d\'ajouter un nouvel email',
     'tr_emails_management_invalid_image_type' => 'Type d\'image incorrect',
-    'tr_emails_management_invalid_image_width' => 'La largeur de l\'image doit être 800 pixels ou moins',
+    'tr_emails_management_invalid_image_width' => 'La largeur de l\'image doit être de 800 pixels ou moins',
+    'tr_emails_management_invalid_image_height' => 'La hauteur de l\'image doit être de 800 pixels ou moins',
     'tr_emails_management_invalid_image_size' => 'La taille maximale autorisée pour le fichier est de 250 kilo-octets ou moins',
 
     // Action Messages
