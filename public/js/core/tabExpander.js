fa2d4c30558c1f78016a5b41c727e5970fb0a3bd
Junry Zabate <jzabate@melistechnology.com>
2020-02-13 16:52:07 +0800

    rebundle js

--- public/js/core/tabExpander.js
+++ public/js/core/tabExpander.js
@@ -31,8 +31,8 @@ var tabExpander = (function($, window){
         
         //get the width in % of #melis-navtabs-container-inner based from #melis-navtabs-container-outer - 56px for the prevnext buttons
         
-        var innerUlWidthPercent = 99 - ( (100 * 65) / $("#melis-navtabs-container-outer").outerWidth() ); 
-        // var innerUlWidthPercent = 100 - ( (100 * 56) / $("#melis-navtabs-container-outer").outerWidth() );
+        //var innerUlWidthPercent = 99 - ( (100 * 65) / $("#melis-navtabs-container-outer").outerWidth() ); 
+        var innerUlWidthPercent = 100 - ( (100 * 56) / $("#melis-navtabs-container-outer").outerWidth() );
 
         $("#melis-navtabs-container-inner").css({"width": (innerUlWidthPercent)+"%" , "overflow":"hidden"}); // change hidden to initial because of dropdown
         $navTabs.css({"width": navUlContainer });
