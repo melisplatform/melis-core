c8d3548821d8c392765f58361e8ed578860950e7
Ralph Bryan Brioso <bbrioso@melistechnology.com>
2020-03-30 12:10:55 +0800

    Exception and Not found reason pages updated

--- config/module.config.php
+++ config/module.config.php
@@ -535,8 +535,6 @@ return array(
         ]
     ],
     'view_manager' => [
-//        'display_not_found_reason' => true,
-//        'display_exceptions' => true,
         'doctype' => 'HTML5',
         'not_found_template' => 'error/404',
         'exception_template' => 'error/index',
