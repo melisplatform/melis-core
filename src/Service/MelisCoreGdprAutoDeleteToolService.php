f1b3585d96a0af6b70f85b89d0e12a8ce3262a95
jerremeirago <jrago@melistechnology.com>
2020-05-19 08:18:58 +0000

    fixed ticket https://mantis2.uat.melistechnology.fr/view.php?id=388

--- src/Service/MelisCoreGdprAutoDeleteToolService.php
+++ src/Service/MelisCoreGdprAutoDeleteToolService.php
@@ -497,7 +497,6 @@ class MelisCoreGdprAutoDeleteToolService extends MelisCoreGeneralService
     {
         $data = $this->getGdprAutoDeleteConfigDataById($id);
         // delete logs data
-        $this->gdprAutoDeleteEmailsLogsTable->deleteByField('mgdprl_module_name', $data['mgdprc_module_name']);
 
         return $this->gdprAutoDeleteConfigTable->deleteById($id);
     }
