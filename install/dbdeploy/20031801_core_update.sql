4b5383de44ef69438b1363679e862e80d507fe45
jerremeirago <jrago@melistechnology.com>
2020-07-06 19:38:04 +0800

    fixed sql issues

--- install/dbdeploy/20031801_core_update.sql
+++ install/dbdeploy/20031801_core_update.sql
@@ -1,28 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Apr 06, 2020 at 09:27 AM
--- Server version: 5.7.29-0ubuntu0.16.04.1
--- PHP Version: 7.3.15
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
--- --------------------------------------------------------
-
---
--- Table structure for table `melis_core_gdpr_delete_emails_smtp`
---
-
 CREATE TABLE `melis_core_gdpr_delete_emails_smtp` (
   `mgdpr_smtp_id` int(11) NOT NULL,
   `mgdpr_smtp_host` varchar(255) NOT NULL,
@@ -30,27 +5,9 @@ CREATE TABLE `melis_core_gdpr_delete_emails_smtp` (
   `mgdpr_smtp_password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `melis_core_gdpr_delete_emails_smtp`
---
 ALTER TABLE `melis_core_gdpr_delete_emails_smtp`
-  ADD PRIMARY KEY (`mgdpr_smtp`);
+  ADD PRIMARY KEY (`mgdpr_smtp_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `melis_core_gdpr_delete_emails_smtp`
---
 ALTER TABLE `melis_core_gdpr_delete_emails_smtp`
-  MODIFY `mgdpr_smtp` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `mgdpr_smtp_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
