6d51a1ce65f16e4f00136d269eab5a2bf111f4ad
bbrioso <rbbrioso28@gmail.com>
2020-08-14 11:59:35 +0800

    Generic model getTotalData function update

--- src/Model/Tables/MelisGenericTable.php
+++ src/Model/Tables/MelisGenericTable.php
@@ -35,32 +35,32 @@ class MelisGenericTable extends MelisServiceManager
 	protected $_selectedValues;
 	protected $_currentDataCount;
 
-    /**
-     * @param TableGateway $tableGateway
-     */
-    public function setTableGateway(TableGateway $tableGateway)
-    {
-        $this->tableGateway = $tableGateway;
-    }
-
-    /**
-     * @return TableGateway $tableGateway
-     */
-	public function getTableGateway()
+	/**
+	 * @param TableGateway $tableGateway
+	 */
+	public function setTableGateway(TableGateway $tableGateway)
 	{
-	    return $this->tableGateway;
+		$this->tableGateway = $tableGateway;
 	}
 
-    /**
-     * @return HydratingResultSet
-     */
-    public function hydratingResultSet()
-    {
-        return $hydratingResultSet = new HydratingResultSet(new ObjectProperty(), new MelisResultSet());
-    }
+	/**
+	 * @return TableGateway $tableGateway
+	 */
+	public function getTableGateway()
+	{
+		return $this->tableGateway;
+	}
+
+	/**
+	 * @return HydratingResultSet
+	 */
+	public function hydratingResultSet()
+	{
+		return $hydratingResultSet = new HydratingResultSet(new ObjectProperty(), new MelisResultSet());
+	}
 
 
-    public function fetchAll()
+	public function fetchAll()
 	{
 		$resultSet = $this->getTableGateway()->select();
 		
@@ -108,10 +108,10 @@ class MelisGenericTable extends MelisServiceManager
 	
 	public function update($datas, $whereField, $whereValue)
 	{
-	    if($this->getEntryByField($whereField, $whereValue)->current())
-	    {
-	        $this->getTableGateway()->update($datas, array($whereField => $whereValue));
-	    }
+		if($this->getEntryByField($whereField, $whereValue)->current())
+		{
+			$this->getTableGateway()->update($datas, array($whereField => $whereValue));
+		}
 	}
 	
 	public function getLastInsertId()
@@ -137,10 +137,10 @@ class MelisGenericTable extends MelisServiceManager
 	 */
 	public function getTableColumns()
 	{
-	    $metadata = new MetaData($this->getTableGateway()->getAdapter());
-	    $columns = $metadata->getColumnNames($this->getTableGateway()->getTable());
+		$metadata = new MetaData($this->getTableGateway()->getAdapter());
+		$columns = $metadata->getColumnNames($this->getTableGateway()->getTable());
 	
-	    return $columns;
+		return $columns;
 	}
 	
 	/**
@@ -155,34 +155,34 @@ class MelisGenericTable extends MelisServiceManager
 	public function fetchData($columns = null)
 	{
 	
-	    // auto populate columns with arrays from the existing Table
-	    // when user does not supply a parameter to this function
-	    if($columns == null)
-	    {
-	        $this->_selectedColumns = $this->getTableColumns();
-	    }
-	    else
-	    {
-	        if(is_array($columns))
-	        {
-	            $this->_selectedColumns = $columns;
-	        }
-	        else
-	        {
-	            throw new \InvalidArgumentException('Invalid argument provided on Column parameter');
-	        }
-	    }
+		// auto populate columns with arrays from the existing Table
+		// when user does not supply a parameter to this function
+		if($columns == null)
+		{
+			$this->_selectedColumns = $this->getTableColumns();
+		}
+		else
+		{
+			if(is_array($columns))
+			{
+				$this->_selectedColumns = $columns;
+			}
+			else
+			{
+				throw new \InvalidArgumentException('Invalid argument provided on Column parameter');
+			}
+		}
 	
-	    $select = new Select();
+		$select = new Select();
 	
-	    $select->columns(array_keys($this->_selectedColumns));
-	    $select->from($this->getTableGateway()->getTable());
-	    $resultSet = $this->getTableGateway()->selectWith($select);
-	    $resultSet->buffer();
-	    $this->_selectedValues = $resultSet;
+		$select->columns(array_keys($this->_selectedColumns));
+		$select->from($this->getTableGateway()->getTable());
+		$resultSet = $this->getTableGateway()->selectWith($select);
+		$resultSet->buffer();
+		$this->_selectedValues = $resultSet;
 	
-	    //return $resultSet = $this->getSelectedValues();
-	    return $resultSet;
+		//return $resultSet = $this->getSelectedValues();
+		return $resultSet;
 	
 	}
 	
@@ -192,130 +192,130 @@ class MelisGenericTable extends MelisServiceManager
 	 */
 	public function getSelectedColumns()
 	{
-	    if(!empty($this->_selectedColumns))
-	    {
-	        return $this->_selectedColumns;
-	    }
-	    else
-	    {
-	        return $this->getTableColumns();
-	    }
+		if(!empty($this->_selectedColumns))
+		{
+			return $this->_selectedColumns;
+		}
+		else
+		{
+			return $this->getTableColumns();
+		}
 	}
 	
 	/**
 	 * This is used whenever you want to implement a pagination on your data table
 	 * @tutorial Array Structure
 	 * array(
-     *           'where' => array(
-     *               'key' => 'pros_id',
-     *               'value' => $search,
-     *           ),
-     *           'order' => array(
-     *               'key' => $selCol,
-     *               'dir' => $sortOrder,
-     *           ),
-     *           'start' => $start,
-     *           'limit' => $length,
-     *           'columns' => $colId
-     *       )
+	 *           'where' => array(
+	 *               'key' => 'pros_id',
+	 *               'value' => $search,
+	 *           ),
+	 *           'order' => array(
+	 *               'key' => $selCol,
+	 *               'dir' => $sortOrder,
+	 *           ),
+	 *           'start' => $start,
+	 *           'limit' => $length,
+	 *           'columns' => $colId
+	 *       )
 	 * @param array $options
 	 * @param array $fixedCriteria (optional)
 	 * @return array
 	 */
 	public function getPagedData(array $options, $fixedCriteria = null)
 	{
-	    $select = $this->getTableGateway()->getSql()->select();
+		$select = $this->getTableGateway()->getSql()->select();
 
-	    $where = !empty($options['where']['key']) ? $options['where']['key'] : '';
-	    $whereValue = !empty($options['where']['value']) ? $options['where']['value'] : '';
+		$where = !empty($options['where']['key']) ? $options['where']['key'] : '';
+		$whereValue = !empty($options['where']['value']) ? $options['where']['value'] : '';
 	
-	    $order = !empty($options['order']['key']) ? $options['order']['key'] : '';
-	    $orderDir = !empty($options['order']['dir']) ? $options['order']['dir'] : 'ASC';
+		$order = !empty($options['order']['key']) ? $options['order']['key'] : '';
+		$orderDir = !empty($options['order']['dir']) ? $options['order']['dir'] : 'ASC';
 	
-	    $start = (int) $options['start'];
-	    $limit = (int) $options['limit'] === -1 ? $this->getTotalData() : (int) $options['limit'];
+		$start = (int) $options['start'];
+		$limit = (int) $options['limit'] === -1 ? $this->getTotalData() : (int) $options['limit'];
 
-	    if(isset($options['status'])) {
-            switch ($options['status']) {
-                case 'ACTIVE' :
-                    $status = 1;
-                    break;
-                case 'INACTIVE' :
-                    $status = 0;
-                    break;
-                case 'PENDING' :
-                    $status = 2;
-                    break;
-                default :
-                    $status = null;
-            }
-        } else {
-            $status = null;
-        }
+		if(isset($options['status'])) {
+			switch ($options['status']) {
+				case 'ACTIVE' :
+					$status = 1;
+					break;
+				case 'INACTIVE' :
+					$status = 0;
+					break;
+				case 'PENDING' :
+					$status = 2;
+					break;
+				default :
+					$status = null;
+			}
+		} else {
+			$status = null;
+		}
 	
-	    $columns = $options['columns'];
-	    
-	    // check if there's an extra variable that should be included in the query
-	    $dateFilter = $options['date_filter'];
-	    $dateFilterSql = '';
-	    
-	    if(count($dateFilter)) {
-	        if(!empty($dateFilter['startDate']) && !empty($dateFilter['endDate'])) {
-	            $dateFilterSql = '`' . $dateFilter['key'] . '` BETWEEN \'' . $dateFilter['startDate'] . '\' AND \'' . $dateFilter['endDate'] . '\'';
-	        }
-	    }
+		$columns = $options['columns'];
+		
+		// check if there's an extra variable that should be included in the query
+		$dateFilter = $options['date_filter'];
+		$dateFilterSql = '';
+		
+		if(count($dateFilter)) {
+			if(!empty($dateFilter['startDate']) && !empty($dateFilter['endDate'])) {
+				$dateFilterSql = '`' . $dateFilter['key'] . '` BETWEEN \'' . $dateFilter['startDate'] . '\' AND \'' . $dateFilter['endDate'] . '\'';
+			}
+		}
 
-        // this is used when searching
-	    if(!empty($where) && !empty($whereValue)) {
-	        $w = new Where();
-	        $p = new PredicateSet();
-	        $filters = array();
-	        $likes = array();
-	        foreach($columns as $colKeys) {
-                if($colKeys != "usr_status")
-	                $likes[] = new Like($colKeys, '%'.$whereValue.'%');
-	        }
-	        
-	        if(!empty($dateFilterSql)) {
-	            $filters = array(new PredicateSet($likes,PredicateSet::COMBINED_BY_OR), new \Laminas\Db\Sql\Predicate\Expression($dateFilterSql));
-	        } else {
-	            $filters = array(new PredicateSet($likes,PredicateSet::COMBINED_BY_OR));
-	        }
+		// this is used when searching
+		if(!empty($where) && !empty($whereValue)) {
+			$w = new Where();
+			$p = new PredicateSet();
+			$filters = array();
+			$likes = array();
+			foreach($columns as $colKeys) {
+				if($colKeys != "usr_status")
+					$likes[] = new Like($colKeys, '%'.$whereValue.'%');
+			}
+			
+			if(!empty($dateFilterSql)) {
+				$filters = array(new PredicateSet($likes,PredicateSet::COMBINED_BY_OR), new \Laminas\Db\Sql\Predicate\Expression($dateFilterSql));
+			} else {
+				$filters = array(new PredicateSet($likes,PredicateSet::COMBINED_BY_OR));
+			}
 
-	        $fixedWhere = array(new PredicateSet(array(new Operator('', '=', ''))));
-	        if(is_null($fixedCriteria)) {
-	            $select->where($filters);
-	        } else {
-	            $select->where(array(
-	                $fixedWhere,
-	                $filters,
-	            ), PredicateSet::OP_AND);
-	        }
-	    }
+			$fixedWhere = array(new PredicateSet(array(new Operator('', '=', ''))));
+			if(is_null($fixedCriteria)) {
+				$select->where($filters);
+			} else {
+				$select->where(array(
+					$fixedWhere,
+					$filters,
+				), PredicateSet::OP_AND);
+			}
+		}
 
-        if(!is_null($status))
-            $select->where("usr_status = ".$status );
+		if(!is_null($status))
+			$select->where("usr_status = ".$status );
 
-	    // used when column ordering is clicked
-	    if(!empty($order))
-	        $select->order($order . ' ' . $orderDir);
+		// used when column ordering is clicked
+		if(!empty($order))
+			$select->order($order . ' ' . $orderDir);
 
 //        $artistTable = new TableGateway($this->$this->getTableGateway()->getTable(), $this->$this->getTableGateway()->getAdapter());
 //        $res = $artistTable->selectWith($select);
 //        dd($res->count());
 
-	    $getCount = $this->getTableGateway()->selectWith($select);
-	    $this->setCurrentDataCount((int) $getCount->getFieldCount());
+		$getCount = $this->getTableGateway()->selectWith($select);
+		$this->setCurrentDataCount((int) $getCount->getFieldCount());
 
 
-        // this is used in paginations
-        $select->limit($limit);
-        $select->offset($start);
+		// this is used in paginations
+		$select->limit($limit);
+		$select->offset($start);
 
 
-        $resultSet = $this->getTableGateway()->selectWith($select);
-        
-        return $resultSet;
+		$resultSet = $this->getTableGateway()->selectWith($select);
+		
+		return $resultSet;
 	}
 	
 	
@@ -325,16 +325,16 @@ class MelisGenericTable extends MelisServiceManager
 	 */
 	public function getTotalData($field = null, $idValue = null)
 	{
-	    $select = $this->getTableGateway()->getSql()->select();
-	    
-	    if (!empty($field) && !empty($idValue))
-	    {
-	    	$select->where(array($field => (int) $idValue));
-	    }
-	    
-	    $resultSet = $this->getTableGateway()->selectWith($select);
-
-	    return $resultSet->getFieldCount();
+		$select = $this->getTableGateway()->getSql()->select();
+		
+		if (!empty($field) && !empty($idValue))
+		{
+			$select->where(array($field => (int) $idValue));
+		}
+		
+		$resultSet = $this->getTableGateway()->selectWith($select);
+		
+		return $resultSet->count();
 	}
 	
 	/**
@@ -343,7 +343,7 @@ class MelisGenericTable extends MelisServiceManager
 	 */
 	public function getTotalFiltered() 
 	{
-        return $this->_currentDataCount;
+		return $this->_currentDataCount;
 	}
 	
 	/**
@@ -354,26 +354,26 @@ class MelisGenericTable extends MelisServiceManager
 	 */
 	public function getDataForExport($filter, $columns = array())
 	{
-        $select = $this->getTableGateway()->getSql()->select();
-        $select->columns(array('*'));
-        
-        $w = new Where();
-        $filters = array();
-        $likes = array();
-        
-        foreach($columns as $columnKeys) 
-        {
-            $likes[] = new Like($columnKeys, '%'.$filter.'%');
-        }
-        
-        $filters = array(new PredicateSet($likes, PredicateSet::COMBINED_BY_OR));
-        
-        $select->where($filters);
-        
-        $resultSet = $this->getTableGateway()->selectWith($select);
-        
-        return $resultSet;
-        
+		$select = $this->getTableGateway()->getSql()->select();
+		$select->columns(array('*'));
+		
+		$w = new Where();
+		$filters = array();
+		$likes = array();
+		
+		foreach($columns as $columnKeys) 
+		{
+			$likes[] = new Like($columnKeys, '%'.$filter.'%');
+		}
+		
+		$filters = array(new PredicateSet($likes, PredicateSet::COMBINED_BY_OR));
+		
+		$select->where($filters);
+		
+		$resultSet = $this->getTableGateway()->selectWith($select);
+		
+		return $resultSet;
+		
 	}
 	
 	/**
@@ -382,26 +382,26 @@ class MelisGenericTable extends MelisServiceManager
 	 */
 	protected function getSelectedValues()
 	{
-	    $resultSet = array();
-	    foreach($this->_selectedValues as $keys => $values)
-	    {
-	        // cast object into array
-	        $resultSet[] = (array)$values;
-	    }
-	    return $resultSet;
+		$resultSet = array();
+		foreach($this->_selectedValues as $keys => $values)
+		{
+			// cast object into array
+			$resultSet[] = (array)$values;
+		}
+		return $resultSet;
 	
 	}
 	
 	protected function setCurrentDataCount($dataCount) 
 	{
-	   $this->_currentDataCount = $dataCount;   
+	$this->_currentDataCount = $dataCount;   
 	}
 	
 	protected function getRawSql($select)
 	{
-	    $sql = $this->getTableGateway()->getSql();
-	    $raw = $sql->getSqlstringForSqlObject($select);
+		$sql = $this->getTableGateway()->getSql();
+		$raw = $sql->getSqlstringForSqlObject($select);
 	
-	    return $raw;
+		return $raw;
 	}
 }
