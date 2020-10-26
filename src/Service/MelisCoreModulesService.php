c6732ac344ab9063dc471b075224912a57360867
bbrioso <rbbrioso28@gmail.com>
2020-10-26 12:03:10 +0800

    src/Service/Melis = get_object_vars();

--- src/Service/MelisCoreModulesService.php
+++ src/Service/MelisCoreModulesService.php
@@ -251,15 +251,19 @@ class MelisCoreModulesService extends MelisServiceManager
             $type = $package->type;
             $extra = $package->extra ?? [];
             $isMelisModule = true;
-            if (array_key_exists('melis-module', $extra)) {
-                $key = 'melis-module';
-                if (!$extra->$key)
-                    $isMelisModule = false;
-            }
 
-            /** @var CompletePackage $package */
-            return $type === 'melisplatform-module' &&
-                array_key_exists('module-name', $extra) && $isMelisModule;
+            if (!empty($extra)) {
+
+                if (property_exists($extra, 'melis-module')) {
+                    $key = 'melis-module';
+                    if (!$extra->$key)
+                        $isMelisModule = false;
+                }
+
+                /** @var CompletePackage $package */
+                return $type === 'melisplatform-module' &&
+                    property_exists($extra, 'module-name') && $isMelisModule;
+            }
         });
 
         $modules = array_map(function ($package) {
