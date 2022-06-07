35fb13dbb8d4f9047e1b703b9d09fdcb073d9f16
Cris <crismakabenta@gmail.com>
2022-06-07 18:06:02 +0800

    Replace tab with space as per psr2

--- language/fr_FR.forms.php
+++ language/fr_FR.forms.php
@@ -1,89 +1,89 @@
 <?php 
-	return array(  
-		'tr_meliscore_login_form_Login' => 'Login / Email',
-        'tr_meliscore_login_form_Password' => 'Mot de passe',
-        'tr_meliscore_login_form_submittext_Connect' => 'Connexion',
-        'tr_meliscore_login_header_Logout' => 'Déconnexion',
-        
-        'tr_meliscore_forgot_form_email' => 'Email',
-        'tr_meliscore_forgot_form_email_required' => 'Email *',
-        'tr_meliscore_forgot_form_login' => 'Login',
-        'tr_meliscore_forgot_form_login_required' => 'Login *',
-        'tr_meliscore_forgot_form_submit' => 'Envoyer',
-        'tr_meliscore_forgot_page_title' => 'Mot de passe oublié - ',
-        'tr_meliscore_forgot_page_header' => 'Récupérer votre compte',
-        
-        'tr_meliscore_reset_password' => 'Mot de passe',
-        'tr_meliscore_reset_password_required' => 'Mot de passe *',
-        'tr_meliscore_reset_password_confirm' => 'Confirmer le mot de passe',
-        'tr_meliscore_reset_password_confirm_required' => 'Confirmer le mot de passe *',
-        'tr_meliscore_reset_password_button' => 'Réinitialiser',
-        
-        'tr_meliscore_login_errors_Empty datas' => 'Données vides',
-        'tr_meliscore_login_auth_Successful authentication' => 'Authentification réussie',
-        'tr_meliscore_login_auth_Failed authentication' => 'Echec de l\'authentification',
-        'tr_meliscore_login_auth_failed_inactive' => 'Unable to login, inactive user',
-        'tr_meliscore_login_pass_placeholder' => '(Ne pas remplir pour conserver le mot de passe actuel)',
-        
-        'tr_meliscore_tool_user_form_language' => 'Langage',
-        'tr_meliscore_tool_user_form_language tooltip' => 'Langage dans lequel sera affiché le Back Office pour l&#39;utilisateur',
-        
-        // login prompts
-        'tr_meliscore_login_error' => 'Login ou Mot de passe incorrect',
-        
-        // User Tool prompts
-        'tr_meliscore_tool_user_usr_login_error_empty' => 'Veuillez saisir un login',
-        'tr_meliscore_tool_user_usr_login_error_long'  => 'Login trop long, il doit faire moins de 255 caractères',
-        'tr_meliscore_tool_user_usr_login_invalid' => 'Nom d&#39;utilisateur invalide, veuillez en choisir un autre',
-        
-        'tr_meliscore_tool_user_usr_status_error_empty' => 'Veuillez sélectionner un statut',
-        'tr_meliscore_tool_user_usr_status_error_invalid'  => 'Statut sélectionné invalide',
-        
-        'tr_meliscore_tool_user_usr_email_error_empty' => 'Veuillez saisir une adresse email',
-        'tr_meliscore_tool_user_usr_email_error_long'  => 'Adresse email trop longue, elle doit faire moins de 255 caractères',
-        'tr_meliscore_tool_user_invalid_email' => 'Format invalide',
-        
-        'tr_meliscore_tool_user_usr_password_error_empty' => 'Veuillez saisir un mot de passe',
-        'tr_meliscore_tool_user_usr_password_error_long'  => 'Mot de passe trop long, il doit faire moins de 255 caractères',
-        'tr_meliscore_tool_user_usr_password_not_match' => 'Les deux mots de passe ne sont pas identiques',
-        'tr_meliscore_tool_user_usr_password_regex_not_match' => 'Votre mot de passe doit être de 8 caractères ou plus avec au moins une lettre minuscule et un chiffre',
-        'tr_meliscore_tool_user_usr_password_error_low' => 'Le mot de passe doit être de 8 caractères minimum',
-        
-        'tr_meliscore_tool_user_usr_confirm_password_error_empty' => 'Veuillez confirmer le mot de passe',
-        'tr_meliscore_tool_user_usr_confirm_password_error_long'  => 'Mot de passe (2) trop long, il doit faire moins de 255 caractères',
-        'tr_meliscore_tool_user_usr_confirm_password_error_low' => 'Le mot de passe doit être de 8 caractères minimum',
-        
-        'tr_meliscore_tool_user_usr_firstname_error_empty' => 'Veuillez saisir un prénom',
-        'tr_meliscore_tool_user_usr_firstname_invalid' => 'Prénom saisi invalide',
-        'tr_meliscore_tool_user_usr_firstname_error_long'  => 'Prénom trop long, il doit faire moins de 255 caractères',
-        
-        'tr_meliscore_tool_user_usr_lastname_error_empty' => 'Veuillez saisir un nom',
-        'tr_meliscore_tool_user_usr_lastname_invalid' => 'Nom saisi invalide',
-        'tr_meliscore_tool_user_usr_lastname_error_long'  => 'Nom trop long, il doit faire moins de 255 caractères',
-        
-        'tr_meliscore_tool_user_usr_lang_id_error_empty' => 'Veuillez choisir un langage',
-        'tr_meliscore_tool_user_usr_lang_id_error_invalid'  => 'Langage sélectionné invalide',
-        
-        'tr_meliscore_tool_user_usr_image_error_large' => 'Image trop volumineuse, elle doit être de moins de 16MB',
-        'tr_meliscore_tool_user_usr_image_error_invalid'  => 'Format de l\'image invalide, veuillez en choisir un autre',
-        
-        'tr_meliscore_tool_user_usr_profile_image' => 'Photo de profil',
-        'tr_meliscore_tool_user_usr_file_button' => 'Sélectionner une image',
-	    
-	    'tr_meliscore_user_password_change_succes' => 'Mot de passe modifié avec succès.',
+return array(
+    'tr_meliscore_login_form_Login' => 'Login / Email',
+    'tr_meliscore_login_form_Password' => 'Mot de passe',
+    'tr_meliscore_login_form_submittext_Connect' => 'Connexion',
+    'tr_meliscore_login_header_Logout' => 'Déconnexion',
 
-        'tr_meliscore_login_password_enc_update' => 'Nous avons changé notre algorithme de cryptage, veuillez mettre à jour votre mot de passe pour sécuriser votre compte. Pour cela cliquez sur <strong>Mot de passe oublié</strong> pour créer un nouveau mot de passe. Nous nous excusons pour la gêne occasionnée.',
+    'tr_meliscore_forgot_form_email' => 'Email',
+    'tr_meliscore_forgot_form_email_required' => 'Email *',
+    'tr_meliscore_forgot_form_login' => 'Login',
+    'tr_meliscore_forgot_form_login_required' => 'Login *',
+    'tr_meliscore_forgot_form_submit' => 'Envoyer',
+    'tr_meliscore_forgot_page_title' => 'Mot de passe oublié - ',
+    'tr_meliscore_forgot_page_header' => 'Récupérer votre compte',
 
-        // Platform Scheme
-        'tr_meliscore_platform_scheme_primary_tooltip' => 'Change la couleur principale de la plateforme en saisissant le code hexadécimal ou en utilisant le selecteur de couleur',
-        'tr_meliscore_platform_scheme_secondary_tooltip' => 'Change la couleur secondaire de la plateforme en saisissant le code hexadécimal ou en utilisant le selecteur de couleur',
-        'tr_meliscore_platform_scheme_favicon_tooltip' => 'Change le favicon du Back Office (taille: 16 x 16)',
-        'tr_meliscore_platform_scheme_login_logo_tooltip' => 'Change le logo du login du Back Office (taille: 120 x 120)',
-        'tr_meliscore_platform_scheme_login_background_tooltip' => 'Change l\'arrière-plan du login du Back Office (taille: 1920 x 1080)',
-        'tr_meliscore_platform_scheme_sidebar_header_logo_tooltip' => 'Change le logo en haut à gauche de la plateforme (taille: 30 x 30)',
-        'tr_meliscore_platform_scheme_sidebar_header_title_tooltip' => 'Change le titre en haut à gauche de la plateforme',
-        'tr_meliscore_platform_scheme_file_button' => 'Choisir une image',
-        'tr_melis_core_platform_color_sidebar_bg_color_tooltip' => 'Change la couleur de fond du menu de gauche soit en saisissant le code hexadécimal soit en utilisant le sélecteur de couleur',
-        'tr_melis_core_platform_color_login_link_color_tooltip' => 'Change la couleur des liens Melis Technology et melis Platform dans le login soit en saisissant le code hexadécimal soit en utilisant le sélecteur de couleur',
+    'tr_meliscore_reset_password' => 'Mot de passe',
+    'tr_meliscore_reset_password_required' => 'Mot de passe *',
+    'tr_meliscore_reset_password_confirm' => 'Confirmer le mot de passe',
+    'tr_meliscore_reset_password_confirm_required' => 'Confirmer le mot de passe *',
+    'tr_meliscore_reset_password_button' => 'Réinitialiser',
+
+    'tr_meliscore_login_errors_Empty datas' => 'Données vides',
+    'tr_meliscore_login_auth_Successful authentication' => 'Authentification réussie',
+    'tr_meliscore_login_auth_Failed authentication' => 'Echec de l\'authentification',
+    'tr_meliscore_login_auth_failed_inactive' => 'Unable to login, inactive user',
+    'tr_meliscore_login_pass_placeholder' => '(Ne pas remplir pour conserver le mot de passe actuel)',
+
+    'tr_meliscore_tool_user_form_language' => 'Langage',
+    'tr_meliscore_tool_user_form_language tooltip' => 'Langage dans lequel sera affiché le Back Office pour l&#39;utilisateur',
+
+    // login prompts
+    'tr_meliscore_login_error' => 'Login ou Mot de passe incorrect',
+
+    // User Tool prompts
+    'tr_meliscore_tool_user_usr_login_error_empty' => 'Veuillez saisir un login',
+    'tr_meliscore_tool_user_usr_login_error_long'  => 'Login trop long, il doit faire moins de 255 caractères',
+    'tr_meliscore_tool_user_usr_login_invalid' => 'Nom d&#39;utilisateur invalide, veuillez en choisir un autre',
+
+    'tr_meliscore_tool_user_usr_status_error_empty' => 'Veuillez sélectionner un statut',
+    'tr_meliscore_tool_user_usr_status_error_invalid'  => 'Statut sélectionné invalide',
+
+    'tr_meliscore_tool_user_usr_email_error_empty' => 'Veuillez saisir une adresse email',
+    'tr_meliscore_tool_user_usr_email_error_long'  => 'Adresse email trop longue, elle doit faire moins de 255 caractères',
+    'tr_meliscore_tool_user_invalid_email' => 'Format invalide',
+
+    'tr_meliscore_tool_user_usr_password_error_empty' => 'Veuillez saisir un mot de passe',
+    'tr_meliscore_tool_user_usr_password_error_long'  => 'Mot de passe trop long, il doit faire moins de 255 caractères',
+    'tr_meliscore_tool_user_usr_password_not_match' => 'Les deux mots de passe ne sont pas identiques',
+    'tr_meliscore_tool_user_usr_password_regex_not_match' => 'Votre mot de passe doit être de 8 caractères ou plus avec au moins une lettre minuscule et un chiffre',
+    'tr_meliscore_tool_user_usr_password_error_low' => 'Le mot de passe doit être de 8 caractères minimum',
+
+    'tr_meliscore_tool_user_usr_confirm_password_error_empty' => 'Veuillez confirmer le mot de passe',
+    'tr_meliscore_tool_user_usr_confirm_password_error_long'  => 'Mot de passe (2) trop long, il doit faire moins de 255 caractères',
+    'tr_meliscore_tool_user_usr_confirm_password_error_low' => 'Le mot de passe doit être de 8 caractères minimum',
+
+    'tr_meliscore_tool_user_usr_firstname_error_empty' => 'Veuillez saisir un prénom',
+    'tr_meliscore_tool_user_usr_firstname_invalid' => 'Prénom saisi invalide',
+    'tr_meliscore_tool_user_usr_firstname_error_long'  => 'Prénom trop long, il doit faire moins de 255 caractères',
+
+    'tr_meliscore_tool_user_usr_lastname_error_empty' => 'Veuillez saisir un nom',
+    'tr_meliscore_tool_user_usr_lastname_invalid' => 'Nom saisi invalide',
+    'tr_meliscore_tool_user_usr_lastname_error_long'  => 'Nom trop long, il doit faire moins de 255 caractères',
+
+    'tr_meliscore_tool_user_usr_lang_id_error_empty' => 'Veuillez choisir un langage',
+    'tr_meliscore_tool_user_usr_lang_id_error_invalid'  => 'Langage sélectionné invalide',
+
+    'tr_meliscore_tool_user_usr_image_error_large' => 'Image trop volumineuse, elle doit être de moins de 16MB',
+    'tr_meliscore_tool_user_usr_image_error_invalid'  => 'Format de l\'image invalide, veuillez en choisir un autre',
+
+    'tr_meliscore_tool_user_usr_profile_image' => 'Photo de profil',
+    'tr_meliscore_tool_user_usr_file_button' => 'Sélectionner une image',
+
+    'tr_meliscore_user_password_change_succes' => 'Mot de passe modifié avec succès.',
+
+    'tr_meliscore_login_password_enc_update' => 'Nous avons changé notre algorithme de cryptage, veuillez mettre à jour votre mot de passe pour sécuriser votre compte. Pour cela cliquez sur <strong>Mot de passe oublié</strong> pour créer un nouveau mot de passe. Nous nous excusons pour la gêne occasionnée.',
+
+    // Platform Scheme
+    'tr_meliscore_platform_scheme_primary_tooltip' => 'Change la couleur principale de la plateforme en saisissant le code hexadécimal ou en utilisant le selecteur de couleur',
+    'tr_meliscore_platform_scheme_secondary_tooltip' => 'Change la couleur secondaire de la plateforme en saisissant le code hexadécimal ou en utilisant le selecteur de couleur',
+    'tr_meliscore_platform_scheme_favicon_tooltip' => 'Change le favicon du Back Office (taille: 16 x 16)',
+    'tr_meliscore_platform_scheme_login_logo_tooltip' => 'Change le logo du login du Back Office (taille: 120 x 120)',
+    'tr_meliscore_platform_scheme_login_background_tooltip' => 'Change l\'arrière-plan du login du Back Office (taille: 1920 x 1080)',
+    'tr_meliscore_platform_scheme_sidebar_header_logo_tooltip' => 'Change le logo en haut à gauche de la plateforme (taille: 30 x 30)',
+    'tr_meliscore_platform_scheme_sidebar_header_title_tooltip' => 'Change le titre en haut à gauche de la plateforme',
+    'tr_meliscore_platform_scheme_file_button' => 'Choisir une image',
+    'tr_melis_core_platform_color_sidebar_bg_color_tooltip' => 'Change la couleur de fond du menu de gauche soit en saisissant le code hexadécimal soit en utilisant le sélecteur de couleur',
+    'tr_melis_core_platform_color_login_link_color_tooltip' => 'Change la couleur des liens Melis Technology et melis Platform dans le login soit en saisissant le code hexadécimal soit en utilisant le sélecteur de couleur',
     );
 ?>
