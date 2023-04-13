3ea1426ad3cd8b7fdd7431c525cf535e6cdedc6c
wongandy <wong_andy31@yahoo.com>
2023-04-13 19:49:14 +0800

    created controller

---
+++ src/Controller/MelisCoreOtherConfigController.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Melis Technology (http://www.melistechnology.com)
+ *
+ * @copyright Copyright (c) 2016 Melis Technology (http://www.melistechnology.com)
+ *
+ */
+
+namespace MelisCore\Controller;
+
+use Laminas\View\Model\ViewModel;
+use Laminas\Session\Container;
+
+use MelisCore\Service\MelisCoreRightsService;
+
+/**
+ * This class renders Melis CMS Dashboard
+*/
+class MelisCoreOtherConfigController extends MelisAbstractActionController
+{
+	/**
+	 * Shows the leftmenu dasboard entry point
+	 * 
+	 * @return \Laminas\View\Model\ViewModel
+	 */
+	public function leftmenuDashboardAction()
+	{
+		$melisKey = $this->params()->fromRoute('melisKey', '');
+		
+		$view = new ViewModel();
+		$view->melisKey = $melisKey;
+		
+		return $view;
+	}
+}
