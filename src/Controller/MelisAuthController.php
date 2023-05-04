dff152411c79dccf9e6722d6686d0976eba604ca
wongandy <wong_andy31@yahoo.com>
2023-05-04 12:14:53 +0800

    added condition to check if password_validity_lifetime is set inside config file

--- src/Controller/MelisAuthController.php
+++ src/Controller/MelisAuthController.php
@@ -263,12 +263,14 @@ class MelisAuthController extends MelisAbstractActionController
                                     $configFactory =  new \Laminas\Config\Factory();
                                     $config = $configFactory->fromFile($file);
 
-                                    $passwordValidityLifetime = $config['password_validity_lifetime'];
-                                    $passwordExpiryDate = date('Y-m-d H:i:s', strtotime($userLastPasswordUpdatedDate . '+' . $passwordValidityLifetime . ' days'));
-                                    $currentDate = date('Y-m-d H:i:s');
-
-                                    if (strtotime($currentDate) > strtotime($passwordExpiryDate)) {
-                                        $isPassExpired = true;
+                                    if (isset($config['password_validity_lifetime'])) {
+                                        $passwordValidityLifetime = $config['password_validity_lifetime'];
+                                        $passwordExpiryDate = date('Y-m-d H:i:s', strtotime($userLastPasswordUpdatedDate . '+' . $passwordValidityLifetime . ' days'));
+                                        $currentDate = date('Y-m-d H:i:s');
+                                        
+                                        if (strtotime($currentDate) > strtotime($passwordExpiryDate)) {
+                                            $isPassExpired = true;
+                                        }
                                     }
                                 }
                                 
