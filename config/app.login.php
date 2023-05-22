27655323a9e348f876fa03d192ce3416d6c1a93b
wongandy <wong_andy31@yahoo.com>
2023-05-22 18:54:02 +0800

    added french translations

--- config/app.login.php
+++ config/app.login.php
@@ -5,8 +5,8 @@ return array(
     'password_duplicate_status' => '0',
     'password_duplicate_lifetime' => '',
     'password_complexity_number_of_characters' => '8',
-    'password_complexity_use_special_characters' => '1',
-    'password_complexity_use_lower_case' => '0',
-    'password_complexity_use_upper_case' => '0',
-    'password_complexity_use_digit' => '0',
+    'password_complexity_use_special_characters' => '',
+    'password_complexity_use_lower_case' => '1',
+    'password_complexity_use_upper_case' => '',
+    'password_complexity_use_digit' => '1',
 );
