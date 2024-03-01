5a6d1905b94ca5ecd2e63bce4f0704a9ddcba12f
jzabate <jzabate@melistechnology.com>
2024-03-01 16:51:58 +0800

    fix issue 6102

--- public/js/tinyMCE/tool.php
+++ public/js/tinyMCE/tool.php
@@ -12,7 +12,7 @@
 		'inline' => false,
 		'menubar' => false,
 		'mini_templates_url' => '/melis/MelisCore/MelisTinyMce/getTinyTemplates',
-		'forced_root_block' => 'div',
+		'forced_root_block' => 'p',
 		//'paste_word_valid_elements'=> "p,b,strong,i,em,h1,h2,h3,h4",
 		'cleanup' => false,
 		'verify_html' => false,
