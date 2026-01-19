67ef95825fa6f4dabbe0ff4b554a4754e247afab
wongandy <wong_andy31@yahoo.com>
2026-01-19 19:56:13 +0800

    unified password requirement

--- config/app.login.php
+++ config/app.login.php
@@ -18,7 +18,7 @@ return array(
                     'password_complexity_number_of_characters' => '8',
                     'password_complexity_use_special_characters' => '',
                     'password_complexity_use_lower_case' => '1',
-                    'password_complexity_use_upper_case' => '',
+                    'password_complexity_use_upper_case' => '1',
                     'password_complexity_use_digit' => '1',
                 ),
             ),
